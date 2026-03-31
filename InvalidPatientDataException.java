3816a489b56d6ac1ec096cba64bce0afaaf0a3c1
Sandesh Paudel <114731627+MrSandSort@users.noreply.github.com>
2026-03-31 11:07:47 +0545

    Add files via upload

---
+++ InvalidPatientDataException.java
@@ -0,0 +1,10 @@
+package Week21;
+
+
+public class InvalidPatientDataException extends RuntimeException 
+{
+    public InvalidPatientDataException(String message) 
+    {
+        super(message);
+    }
+}
